a58099ad6e2ef6917c96dfc2cfff2ab45785cae4
RajaiChetna <rajaichetna@gmail.com>
2025-02-20 16:45:37 +0530

    Initial Commit

--- main.py
+++ main.py
@@ -1,48 +1,10 @@
-#using params key,value
-# from fastapi import FastAPI
-# from pydantic import BaseModel
-# import google.generativeai as genai
-# import psycopg2
-# genai.configure(api_key="AIzaSyBAOoaNksSCkM_AquAMIJghmFPHHsiRl_k")
-# app = FastAPI()
-# # PostgreSQL Connection
-# conn = psycopg2.connect(
-#     dbname="chatbot_db", 
-#     user="postgres", 
-#     password="chet1212", 
-#     host="localhost", 
-#     port="5432"
-# )
-# cursor = conn.cursor()
-
-# class UserInput(BaseModel):
-#     message: str
-
-# @app.post("/chat")
-# def chat_with_gemini(prompt):
-#     try:
-#         model = genai.GenerativeModel("gemini-pro")
-#         response = model.generate_content(prompt)
-
-#         reply = response.text.strip()
-
-#         # Store conversation in PostgreSQL
-#         cursor.execute("INSERT INTO conversations (user_input, bot_reply) VALUES (%s, %s)", (prompt, reply))
-#         conn.commit()
-
-#         return {"Reply": reply}
-
-#     except Exception as e:
-#         raise e
-
-# using body JSON
 from fastapi import FastAPI # Creates an API server
 from pydantic import BaseModel # Define data structure for requests
 import google.generativeai as genai # Google Gemini AI for responses
 import psycopg2 # PostgreSQL database connection
 
 # Configure Google Gemini API
-genai.configure(api_key="AIzaSyBAOoaNksSCkM_AquAMIJghmFPHHsiRl_k") #checks the key
+genai.configure(api_key="your-gemini-key") #checks the key
 
 app = FastAPI() #handle HTTP requests
 
